aee389fdacc5790c247ebbe3f694158a0f1e3aab
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-22 20:16:23 +0900

    GlobalStyle 추가 및 적용
    Co-authored-by: s-woon <s-woon@users.noreply.github.com>

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,14 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    /* background: gray; */
+  }
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+  li {
+    list-style: none;
+  }
+`;
