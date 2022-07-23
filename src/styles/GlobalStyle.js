de8527616d7215b4f31dbe9bbdc6f335db2a9e83
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-24 01:24:14 +0900

    기본 폰트 스타일 추가
    Co-authored-by: s-woon <s-woon@users.noreply.github.com>

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,13 +1,31 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  body {
-    /* background: gray; */
+  @font-face {
+    font-family: 'LeferiPoint-BlackObliqueA';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2201-2@1.0/LeferiPoint-BlackObliqueA.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
   }
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto,
+    'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR',
+    'Malgun Gothic', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol',
+    sans-serif !important;
+  }
+
+  h1 {
+    font-family: 'LeferiPoint-BlackObliqueA';
+  }
+
   a {
     color: inherit;
     text-decoration: none;
   }
+
   li {
     list-style: none;
   }
