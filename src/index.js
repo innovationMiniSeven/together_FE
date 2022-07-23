f1f9d240501454e91924bb4459a12a1177c5ba4f
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-23 20:11:06 +0900

    react-query로 더미 데이터 fetching 받아 홈화면에 렌더링
    Co-authored-by: s-woon <s-woon@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Suspense fallback={<div>로딩중이에요!</div>}>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </Suspense>,
 );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from 'react';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import styled from 'styled-components';
 
 const Home = () => {
+  const getPostList = async () => {
+    const posts = await axios.get('http://localhost:5001/posts');
+    return posts.data;
+  };
+
+  const postList = useQuery(['postList'], getPostList);
+  const category = {
+    PURCHASE: '공동구매',
+    DELIVERY: '배달',
+    EXHIBITION: '공연/전시회',
+    ETC: '기타',
+  };
+
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      {postList.data.map((d) => {
+        return (
+          <div key={d.id}>
+            <p>마감예정일 : {d.deadline}</p>
+            <p>카테고리 : {category[d.category]}</p>
+            <p>제목 : {d.title}</p>
+            <Image src={d.imageUrl} alt=""/>
+            <p>모집인원: {d.currentNumberPeople}/{d.numberPeople}</p>
+            <p>연락방법 : {d.contactMethod}</p>
+            <p>글쓴이 : {d.nickname}</p>
+            <p>조회수 : {d.viewCount}</p>
+            <p>댓글수 : {d.commentCount}</p>
+            <hr />
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Home;
+
+const Image = styled.img`
+  width: 300px;
+`;
