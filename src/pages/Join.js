49d3f2a2462a3490ee4a8ef40e1d5ec04ac908b1
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-25 14:32:39 +0900

    Join 리팩터링 및 에러처리, Form 컴포넌트 분리
    Co-authored-by: s-woon <s-woon@users.noreply.github.com>

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import { AiOutlineArrowRight } from 'react-icons/ai';
+import Form from '../components/Form';
 
 const Join = () => {
   const navigate = useNavigate();
@@ -23,13 +24,20 @@ const Join = () => {
     };
     console.log(data);
 
-    await axios.post('http://localhost:5001/user', data);
-    alert('HeyYo 회원가입 완료! 로그인 페이지로 이동합니다 :)');
-    navigate('/login');
+    try {
+      const res = await axios.post('http://localhost:5001/user', data);
+      // TODO: 백엔드 response 받아서 한번더 검증하는 로직 추가
+      console.log(res);
+      alert('HeyYo 회원가입 완료! 로그인 페이지로 이동합니다 :)');
+      navigate('/login');
+    } catch (err) {
+      console.log(err);
+      alert('회원가입에 문제가 발생했습니다');
+    }
   };
 
   return (
-    <JoinComponent onSubmit={handleSubmit(submitJoin)}>
+    <Form id="signup-form" onSubmit={handleSubmit(submitJoin)}>
       <h2>회원가입</h2>
 
       <InputBox>
@@ -42,7 +50,7 @@ const Join = () => {
             required: '* 필수 입력 값입니다',
             minLength: 4,
           })}
-        ></input>
+        />
         {errors.username && <Error>{errors.username.message}</Error>}
         {errors.username && errors.username.type === 'minLength' && (
           <Error>* 아이디는 4자 이상입니다</Error>
@@ -59,7 +67,7 @@ const Join = () => {
             required: '* 필수 입력 값입니다',
             maxLength: 10,
           })}
-        ></input>
+        />
         {errors.nickname && <Error>{errors.nickname.message}</Error>}
         {errors.nickname && errors.nickname.type === 'maxLength' && (
           <Error>* 닉네임은 10자 이하입니다</Error>
@@ -76,7 +84,7 @@ const Join = () => {
             required: '* 필수 입력 값입니다',
             minLength: 6,
           })}
-        ></input>
+        />
         {errors.password && <Error>{errors.password.message}</Error>}
         {errors.password && errors.password.type === 'minLength' && (
           <Error>* 비밀번호는 6자 이상입니다</Error>
@@ -97,87 +105,31 @@ const Join = () => {
                 '* 비밀번호가 일치하지 않습니다',
             },
           })}
-        ></input>
+        />
         {errors.confirmPassword && (
           <Error>{errors.confirmPassword.message}</Error>
         )}
       </InputBox>
 
-      <JoinBtn type="submit" disabled={!isValid}>
-        HeyYo 가입하기 :)
-      </JoinBtn>
+      <button type="submit" form="signup-form" disabled={!isValid}>
+        가입 HeyYo :)
+      </button>
 
       <p>
         이미 계정이 있다면?
-        <LoginLink to="/login">
+        <Link to="/login">
           로그인하러 가기
           <AiOutlineArrowRight />
-        </LoginLink>
+        </Link>
       </p>
-    </JoinComponent>
+    </Form>
   );
 };
-
-const JoinComponent = styled.form`
-  width: 300px;
-  background-color: #fff;
-  margin: 8vh auto 0;
-  padding: 65px 83px 60px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  gap: 24px;
-  border: 2px solid #f1f1f1;
-  border-radius: 30px;
-  box-shadow: 0px 4px 8px 0px #0000001a;
-  h2 {
-    font-family: 'LeferiPoint-BlackObliqueA' !important;
-  }
-`;
 
 const InputBox = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  label {
-    font-size: 14px;
-    font-weight: 700;
-  }
-  input {
-    padding: 15px 25px;
-    width: 250px;
-    border-radius: 5px;
-    transition: background 0.2s ease-in-out;
-    font-size: 14px;
-    &:hover,
-    &:focus {
-      background: #e5e5e5;
-    }
-  }
-`;
-
-const JoinBtn = styled.button`
-  margin: 5px 0 -15px;
-  width: 100%;
-  padding: 18px 0;
-  border-radius: 5px;
-  background: #f2b90c;
-  font-size: 18px;
-  font-weight: 700;
-  &:disabled {
-    background: #e5e5e5;
-    cursor: default;
-  }
-`;
-
-const LoginLink = styled(Link)`
-  margin-left: 8px;
-  color: #f2a30f;
-  font-weight: 700;
-  &:hover {
-    text-decoration: underline;
-  }
 `;
 
 const Error = styled.p`
