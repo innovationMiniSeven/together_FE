6049b1b5816976bd77ccbd3a8cd0b080f66d9336
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-26 23:42:43 +0900

    포스트 아이템 UI 일부 수정, 이미지 url 검증 후 기본 이미지 적용
    Co-authored-by: s-woon <s-woon@users.noreply.github.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,24 @@ import styled from 'styled-components';
 import { useQuery } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { SERVER_BASE_URL } from '../constants';
 
 const Home = () => {
   const getPostList = async () => {
     const posts = await axios.get('http://localhost:5001/posts');
     return posts.data;
+    // FIXME:
+    // const posts = await axios.get(
+    //   `${SERVER_BASE_URL}/posts?sort=almost&category=ALL&page=0&size=12`,
+    // );
+    // return posts.data.content;
+    // TODO: 무한 스크롤 구현
   };
 
   const postList = useQuery(['postList'], getPostList);
+  // console.log(postList.data);
   const category = useSelector((state) => state.category);
+  const regex = /(http(s))?:\/\/([a-z0-9-]+\.)+[a-z0-9]{2,4}.*$/;
 
   return (
     <>
@@ -36,24 +45,33 @@ const Home = () => {
 
       <PostsSection>
         <PostList>
-          {postList.data.map((d) => {
+          {postList.data.map((d, idx) => {
             return (
-              <PostItem key={d.id}>
+              <PostItem key={idx}>
+                {/* FIXME: key 값 postId로 추후 수정 */}
                 <Link to={`/post/${d.id}`}>
-                  <p>마감예정일 : {d.deadline}</p>
-                  <p>카테고리 : {category[d.category][0]}</p>
-                  <p>제목 : {d.title}</p>
-                  {d.imageUrl ? (
+                  <PostTitle>
+                    <em>
+                      {category[d.category][0]}
+                      <span>
+                        ({d.currentNumberPeople}/{d.numberPeople})
+                      </span>
+                    </em>
+                    <strong>{d.title}</strong>
+                  </PostTitle>
+                  {d.imageUrl && regex.test(d.imageUrl) ? (
                     <Image src={d.imageUrl} alt="" />
                   ) : (
                     <Image src={category[d.category][1]} alt="" />
                   )}
+                  <p>작성자: {d.nickname}</p>
                   <p>
-                    모집인원: {d.currentNumberPeople}/{d.numberPeople}
+                    마감예정일 : {new Date(d.deadline).toLocaleDateString()}
                   </p>
-                  <p>글쓴이 : {d.nickname}</p>
-                  <p>조회수 : {d.viewCount}</p>
-                  <p>댓글수 : {d.commentCount}</p>
+                  <p>
+                    <span>조회수 : {d.viewCount} </span>
+                    <span> 댓글수 : {d.commentCount}</span>
+                  </p>
                 </Link>
               </PostItem>
             );
@@ -135,19 +153,53 @@ const PostItem = styled.li`
   display: flex;
   justify-content: center;
   flex-direction: column;
-  padding: 10px;
-  border: 2px solid #f2f2f2;
+  padding: 20px 15px;
+  border: 3px solid #f2f2f2;
   border-radius: 20px;
   transition: all 0.2s ease-in-out;
+  em {
+    display: block;
+    padding: 3px 7px;
+    border-radius: 5px;
+    background: #f1f1f1;
+    text-align: center;
+    letter-spacing: -0.1em;
+    font-style: normal;
+    font-weight: 600;
+    transition: all 0.4s ease-in-out;
+  }
   &:hover {
     transform: scale(102%);
     background: #f1f1f1;
+    em {
+      background: #ddd;
+    }
   }
-  @media only screen and (max-width: 735px) {
+  strong {
+    overflow: hidden;
+    display: block;
+    width: 300px;
+    margin-top: 15px;
+    font-size: 1.2em;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  @media only screen and (max-width: 763px) {
     width: 100%;
+    align-items: center;
   }
+`;
+
+const PostTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
 `;
 
 const Image = styled.img`
   width: 300px;
+  margin: 10px 0;
+  border-radius: 10px;
+  aspect-ratio: 4 / 3;
+  object-fit: cover;
 `;
