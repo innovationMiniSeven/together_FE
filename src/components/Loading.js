e518f63159861070d6e332f45c07f0eaf72b3cf9
Sooyoung Yoon <ayoooo240@gmail.com>
2022-07-27 21:46:05 +0900

    로딩 스피너 추가
    Co-authored-by: s-woon <s-woon@users.noreply.github.com>

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+import SyncLoader from 'react-spinners/SyncLoader';
 
 const Loading = () => {
   return (
     <>
-      {/* TODO: 로딩스피너 추가 */}
-      <p>Loading 중 ...</p>
+      <LoadingSpinner>
+        <SyncLoader color="#f2a30f" />
+      </LoadingSpinner>
     </>
   );
 };
+
+const LoadingSpinner = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default Loading;
